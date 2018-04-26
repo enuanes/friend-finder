7fa9dd00dd11d88f0ad3407d3ef0f638d7ffbd7e
Eric Nuanes <ecnuanes@outlook.com>
2018-04-25 20:15:25 -0700

    I added dummy data to the friends.js file.

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+// Array that holds all of the friends that have taken the survey
+// Initialize starting with dummy data
+var friendList = [
+    {
+        name: 'Eric Nuanes',
+        profilePic: 'https://orig00.deviantart.net/7681/f/2014/268/b/4/frank_ponch_stamp_by_lunan95-d80g761.jpg',
+        scores: [
+            1,
+            1,
+            5,
+            1,
+            1,
+            4,
+            1,
+            4,
+            4,
+            5
+        ]
+    }
+]
+
+module.exports = friendList;
