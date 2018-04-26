1ea07ec187e758991336bb151ac3e328f25acf3d
Eric Nuanes <ecnuanes@outlook.com>
2018-04-25 20:52:37 -0700

    I fixed an error on the htmlRoutes.js file.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,11 +3,12 @@ var path = require('path');
 
 // Routing
 module.exports = function(app) {
-    // Default GET route that leads to home.html - displays home page
+    // Default GET route that leads to survey.html - displays survey page
     app.get('/survey', function(req, res) {
         res.sendFile(path.join(__dirname + '/../public/survey.html'));
     });
 
+    // Default GET route that leads to home.html - displays home page
     app.use(function(req, res) {
         res.sendFile(path.join(__dirname + '/../public/home.html'));
     });
