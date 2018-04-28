76c5de5c28ce905885ed99986603acf864e453f8
Eric Nuanes <ecnuanes@outlook.com>
2018-04-27 21:08:08 -0700

    I finished adding people to the friends' list array.

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,21 +2,165 @@
 // Initialize starting with dummy data
 var friendList = [
     {
-        name: 'Eric Nuanes',
-        profilePic: 'https://orig00.deviantart.net/7681/f/2014/268/b/4/frank_ponch_stamp_by_lunan95-d80g761.jpg',
+        name: 'Erik Estrada',
+        profilePic: 'https://media3.s-nbcnews.com/j/newscms/2016_27/1141277/erik-estrada-today-160706-01_fb526886664c2f6e8082d38d72e30aac.today-inline-large.jpg',
         scores: [
-            1,
-            1,
-            5,
-            1,
-            1,
-            4,
-            1,
-            4,
-            4,
-            5
+            '1',
+            '1',
+            '3',
+            '1',
+            '1',
+            '4',
+            '5',
+            '4',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'David Hasseslhoff',
+        profilePic: 'http://curpyrfc.org/wp-content/uploads/2018/01/david-hasselhoff-birthday-card-best-of-david-hasselhoff-in-banana-hammock-burn-my-eyes-burn-my-eyes-of-david-hasselhoff-birthday-card.jpg',
+        scores: [
+            '1',
+            '5',
+            '5',
+            '4',
+            '2',
+            '5',
+            '5',
+            '4',
+            '5',
+            '1'
+        ]
+    },
+    {
+        name: 'Bill Cosby',
+        profilePic: 'http://wp.production.patheos.com/blogs/watchinggod/files/2015/07/bill-cosby-2.jpg',
+        scores: [
+            '5',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '5',
+            '1',
+            '4'
+        ]
+    },
+    {
+        name: 'Duane "Dog" Chapman',
+        profilePic: 'https://i.pinimg.com/236x/ac/ec/0f/acec0f1e8f8352ca2153c8c35e0ebbfa.jpg',
+        scores: [
+        '1',
+        '4',
+        '1',
+        '1',
+        '5',
+        '4',
+        '4',
+        '5',
+        '5',
+        '2'
+        ]
+    },
+    {
+        name: 'Kanye West',
+        profilePic: 'https://static.highsnobiety.com/wp-content/uploads/2017/02/23163930/kanye-west-donda-cosmetics-makeup-1-480x320.jpg',
+        scores: [
+        '1',
+        '1',
+        '1',
+        '5',
+        '5',
+        '4',
+        '4',
+        '2',
+        '1',
+        '5'
+        ]
+    },
+    {
+        name: 'Charlie Sheen',
+        profilePic: 'http://img-4.linternaute.com/3_zje5TiGZs-Y-4btn0Qq9T0B0c=/fit-in/620x/smart/c7f81fdc85254d2a82f7307afb892757/ccmcms-linternaute/10190825-charlie-sheen.jpg',
+        scores: [
+        '1',
+        '5',
+        '3',
+        '4',
+        '1',
+        '5',
+        '5',
+        '5',
+        '4',
+        '2'
+        ]
+    },
+    {
+        name: 'Tiger Woods',
+        profilePic: 'https://www.gannett-cdn.com/-mm-/5a4615e546c67c2e54474fc723e67c30040614a2/c=283-0-4645-3280&r=x404&c=534x401/local/-/media/2017/03/20/USATODAY/USATODAY/636256199853106812-XXX-TIGER-WOODS-RD397-89680669.JPG',
+        scores: [
+        '1',
+        '2',
+        '4',
+        '2',
+        '2',
+        '5',
+        '5',
+        '3',
+        '1',
+        '1'      
+        ]
+    },
+    {
+        name: 'Mike "The Situation" Sorrentino',
+        profilePic: 'https://ia.media-imdb.com/images/M/MV5BMTk4NTc0MjE0OF5BMl5BanBnXkFtZTcwMzc4NDE5Mw@@._V1_.jpg',
+        scores: [
+        '1',
+        '5',
+        '3',
+        '4',
+        '2',
+        '5',
+        '5',
+        '2',
+        '1',
+        '5'
+        ]
+    },
+    {
+        name: 'Gary Busey',
+        profilePic: 'https://s.hdnux.com/photos/21/01/31/4462172/19/920x920.jpg',
+        scores: [
+        '1',
+        '3',
+        '1',
+        '1',
+        '5',
+        '5',
+        '5',
+        '5',
+        '1',
+        '5'
+        ]
+    },
+    {
+        name: 'Katt Williams',
+        profilePic: 'http://splitsider.awlnetwork.com/wp-content/uploads/sites/2/2012/12/katt-williams.jpg',
+        scores: [
+        '1',
+        '1',
+        '1',
+        '3',
+        '1',
+        '5',
+        '5',
+        '5',
+        '4',
+        '4'
         ]
     }
-]
+];
 
 module.exports = friendList;
